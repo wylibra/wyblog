2233fe6d329551d8e057c36002820c0f2b34198c
wy <wy_libra@163.com>
2021-05-13 18:59:30 +0800

    feat:utils-test

---
+++ src/utils/test.js
@@ -0,0 +1,39 @@
+/**
+ * 传入数据、操作函数
+ */
+groupBy([6.1, 4.2, 6.3], Math.floor);
+// => { '4': [4.2], '6': [6.1, 6.3] }
+
+function groupBy(arr, fn) {
+  var obj = {};
+  for (var i = 0; i < arr.length; i++) {
+    if (fn(arr[i]) in obj) {
+      obj[fn(arr[i])].push(arr[i]);
+    } else {
+      obj[fn(arr[i])] = [arr[i]];
+    }
+  }
+  console.log(obj);
+}
+
+
+/**
+ * 闭包应用
+ */
+const test = () => { console.log(1) }
+function once(fn) {
+  let flag = true
+  return function () {
+    if (flag) {
+      fn();
+      flag = false;
+    };
+  }
+}
+const func = once(test)
+
+func(); // 输出1
+func(); // 无输出
+func(); // 无输出
+
+
